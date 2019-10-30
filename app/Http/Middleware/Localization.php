74eb91dee0c1cb39052c12791de722470d843aaf
Jermaine Ernst <jermaine.ernst@icloud.com>
2019-10-30 21:18:37 +0100

    Set locale by browser language

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -7,12 +7,27 @@ use Illuminate\Support\Facades\App;
 
 class Localization
 {
+	private const ACCEPTED_LANGUAGES = [
+		'de',
+		'en',
+		'ru',
+	];
+
     public function handle($request, Closure $next)
     {
 	    if (session()->has('locale')) {
 		    App::setLocale(session()->get('locale'));
+
+		    return $next($request);
 	    }
 
-        return $next($request);
+	    $browserLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+	    if (in_array($browserLang, self::ACCEPTED_LANGUAGES)) {
+		    App::setLocale($browserLang);
+
+		    return $next($request);
+	    }
+
+	    return $next($request);
     }
 }
