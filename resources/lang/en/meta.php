c31cb307e0559538f9d3830ad48528ca4f1f5747
Jermaine Ernst <jermaine.ernst@icloud.com>
2019-11-04 13:29:24 +0100

    Add lang pages

---
+++ resources/lang/en/meta.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'title' => 'Nachricht.co - Die verschlüsselte Einweg-Nachricht',
+	'description' => 'Mit Nachricht.co kannst du völlig anonym und verschlüsselt eine sich selbstzerstörende Einweg-Nachricht per Link an deinen Gesprächspartner senden.'
+];
