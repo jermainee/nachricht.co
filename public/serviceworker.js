a90de5c11c730989bb37970134a73f392b83ed78
Jermaine Ernst <jermaine.ernst@icloud.com>
2019-11-04 14:53:50 +0100

    Disable caching

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE = "nachrichtco-cache-v13";
+const CACHE = "nachrichtco-cache-v14";
 const precacheFiles = [];
 
 self.addEventListener("install", function (event) {
@@ -23,62 +23,11 @@ self.addEventListener("activate", function (event) {
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
+    console.log("[ServiceWorker] Skip caching");
+    return; // temporary disable caching
+
     if (event.request.url.includes('/n') || /^https:\/\/nachricht\.co\/(.*?)_(.*?)$/.test(event.request.url)) {
         console.log("[ServiceWorker] Skip caching here because of sensitive data");
         return;
     }
-
-    if (event.request.method !== "GET") return;
-
-    event.respondWith(
-        fromCache(event.request).then(
-            function (response) {
-                // The response was found in the cache so we responde with it and update the entry
-
-                // This is where we call the server to get the newest version of the
-                // file to use the next time we show view
-                event.waitUntil(
-                    fetch(event.request).then(function (response) {
-                        return updateCache(event.request, response);
-                    })
-                );
-
-                return response;
-            },
-            function () {
-                // The response was not found in the cache so we look for it on the server
-                return fetch(event.request)
-                    .then(function (response) {
-                        // If request was success, add or update it in the cache
-                        event.waitUntil(updateCache(event.request, response.clone()));
-
-                        return response;
-                    })
-                    .catch(function (error) {
-                        console.log("[ServiceWorker] Network request failed and no cache." + error);
-                    });
-            }
-        )
-    );
 });
-
-function fromCache(request) {
-    // Check to see if you have it in the cache
-    // Return response
-    // If not in the cache, then return
-    return caches.open(CACHE).then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            if (!matching || matching.status === 404) {
-                return Promise.reject("no-match");
-            }
-
-            return matching;
-        });
-    });
-}
-
-function updateCache(request, response) {
-    return caches.open(CACHE).then(function (cache) {
-        return cache.put(request, response);
-    });
-}
