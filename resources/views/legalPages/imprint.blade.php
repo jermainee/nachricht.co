db6d1576d27661efd2754c9bd4e1a13835deff13
Jermaine Ernst <jermaine.ernst@icloud.com>
2019-11-07 00:33:07 +0100

    Change imprint and privacy email to info@nachricht.co

--- resources/views/legalPages/imprint.blade.php
+++ resources/views/legalPages/imprint.blade.php
@@ -15,7 +15,7 @@
 
     <div class="smallContainer has-margin-top-double has-margin-bottom-double">
         <h2 style="margin-top: 0px;">Angaben gemäß § 5 TMG</h2>
-        <p>Jermaine J. Ernst<br />Elisenstraße 13<br />48317 Drensteinfurt<br />kontakt [ at ] jermaine-ernst.de</p>
+        <p>Jermaine J. Ernst<br />Elisenstraße 13<br />48317 Drensteinfurt<br />info@nachricht.co</p>
 
         <h2>Haftung für Inhalte</h2>
         <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
