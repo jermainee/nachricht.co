27a678df62e5075080e89ef2b7ee219fb185b78f
Jermaine Ernst <jermaine.ernst@icloud.com>
2020-08-16 12:42:51 +0200

    Add dutch language translation

---
+++ resources/lang/nl/frontpage.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+	'hours' => 'uren',
+	'days' => 'Dagen',
+
+	'messagePlaceholder' => 'Typ hier uw bericht...',
+	'deleteAfterReadLabel' => 'Verwijderen na het lezen',
+	'deadlineLabel' => 'Deadline',
+	'passwordLabel' => 'Wachtwoord',
+	'passwordPlaceholder' => 'Wachtwoord (optioneel) ...',
+	'createButton' => 'Bericht aanmaken',
+
+	'copyButton' => 'kopieer link',
+	'copyText' => 'Kopieer de link en stuur hem naar wie je maar wilt.<br/>Het aangemaakte bericht zal worden verwijderd nadat het voor de eerste keer is geopend.',
+	'createNew' => 'Nieuw bericht',
+
+	'openText' => 'Het bericht kan maar één keer worden geopend en vernietigt dan zichzelf.',
+	'openWithPasswordText' => 'Bovendien was het bericht beveiligd met een wachtwoord.',
+	'openPasswordPlaceholder' => 'Voer het wachtwoord in ...',
+	'openButton' => 'Open bericht',
+
+	'welcome' => 'Wat is Nachricht.co?',
+	'welcomeText' => 'Met Nachricht.co kunt u zelfvernietigende en versleutelde eenrichtingsberichten versturen via het internet. Je hoeft niet eens de boodschapper of het sociale netwerk van je keuze te gebruiken. Onafhankelijk, veilig en gratis!',
+
+	'deletion' => 'Volledige verwijdering',
+	'deletionText' => 'Boodschappen vernietigen zichzelf na de eerste opening. Als het bericht niet is geopend, wordt het na 48 uur automatisch verwijderd. Verwijderde berichten kunnen niet worden teruggevonden en worden voor altijd vernietigd.',
+
+	'encryption' => 'Sterke encryptie',
+	'encryptionText' => 'Om berichten te beschermen tegen nieuwsgierige blikken, wordt elk bericht individueel gecodeerd, net als uw verbinding met ons. Hiervoor gebruiken we AES met een sleutellengte van 256 bits, die is goedgekeurd voor overheidsdocumenten met het hoogste niveau van geheimhouding.',
+
+	'anonymity' => 'Anonimiteit',
+	'anonymityText' => 'Wij willen u in staat stellen om zo anoniem en veilig mogelijk te communiceren. Daarom slaan wij geen persoonlijke gegevens of IP-adressen van u op of vragen wij u niet om deze op te slaan.',
+
+	'responsiveness' => 'Ideaal voor smartphones en tablets',
+	'responsivenessText' => 'Om ervoor te zorgen dat u onderweg zo veilig mogelijk kunt schrijven, hebben we Nachricht.co ook voor uw mobiele apparaten geoptimaliseerd - snellere laadtijden en automatische schermaanpassing.',
+
+	'development' => 'Gebruikersgerichte verdere ontwikkeling',
+	'developmentText' => 'Onze gebruikers zijn erg belangrijk voor ons en daarom oriënteren we ons naast de grootst mogelijke veiligheid ook op uw feedback.',
+
+	'userfriendly' => 'Bruikbaarheid',
+	'userfriendlyText' => 'Wij hechten veel belang aan een duidelijk en eenvoudig ontwerp, zodat u uw weg zo gemakkelijk mogelijk kunt vinden. Bovendien worden hyperlinks en smileys automatisch herkend en geconverteerd.',
+
+	'step1' => '<b>Stap 1:</b> Typ uw bericht in het tekstvak bovenaan de pagina en klik op \'Bericht maken\'.',
+	'step2' => '<b>Stap 2:</b> Kopieer de weergegeven link en stuur deze naar de gewenste gesprekspartner - bijvoorbeeld via Facebook of WhatsApp.',
+	'step3' => '<b>Stap 3:</b> Nu klikt de gesprekspartner op de link en kan hij het bericht één keer lezen, omdat het dan zichzelf vernietigt.',
+];
