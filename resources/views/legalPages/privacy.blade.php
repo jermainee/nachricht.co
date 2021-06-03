0809f4a91196802d7b193b075e39902d0b857069
Jermaine Ernst <jermaine.ernst@icloud.com>
2021-06-03 19:29:08 +0200

    Update imprint and privacy address

--- resources/views/legalPages/privacy.blade.php
+++ resources/views/legalPages/privacy.blade.php
@@ -21,7 +21,7 @@
         <h2>2. Allgemeine Hinweise und Pflichtinformationen</h2>
         <h3>Datenschutz</h3> <p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.</p> <p>Wenn Sie diese Website benutzen, werden verschiedene personenbezogene Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können. Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das geschieht.</p> <p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.</p>
         <h3>Hinweis zur verantwortlichen Stelle</h3> <p>Die verantwortliche Stelle für die Datenverarbeitung auf dieser Website ist:</p>
-        <p>Jermaine Ernst<br />Elisenstraße 13<br />48317 Drensteinfurt</p>
+        <p>Jermaine J. Ernst<br />Neckarstraße 6<br />64283 Darmstadt</p>
         <p>E-Mail: info@nachricht.co</p>
         <p>Verantwortliche Stelle ist die natürliche oder juristische Person, die allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, E-Mail-Adressen o. Ä.) entscheidet.</p>
 
