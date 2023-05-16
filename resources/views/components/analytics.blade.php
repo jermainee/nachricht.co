6cb3103d6c7043d424da1417c5d8bf3a118d8a99
Jermaine Jonas Ernst <34961982+jermainee@users.noreply.github.com>
2023-05-16 23:47:32 +0200

    Add cookie consent

--- resources/views/components/analytics.blade.php
+++ resources/views/components/analytics.blade.php
@@ -1,5 +1,3 @@
-<?php if (\App\Http\Util\CookieConsentUtil::isAgreed()): ?>
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-39078917-4"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
@@ -7,4 +5,3 @@
     gtag('js', new Date());
     gtag('config', 'UA-39078917-4', { 'anonymize_ip': true });
 </script>
-<?php endif; ?>
